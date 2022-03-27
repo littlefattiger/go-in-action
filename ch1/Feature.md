417a79f5f2b695087cabbc96cd3987123c3185bc
lilltfattiger <6698381@qq.com>
2022-03-27 01:15:32 -0700

    ch5 done

---
+++ ch1/Feature.md
@@ -0,0 +1,7 @@
+1.1.1 Development speed
+
+1.1.2 Concurrency
+- goroutines
+- channels
+- go type system
+
---
+++ ch5/listing74/listing74.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/goinaction/code/chapter5/listing74/entities"
+)
+
+// main is the entry point for the application.
+func main() {
+	// Create a value of type Admin from the entities package.
+	a := entities.Admin{
+		Rights: 10,
+	}
+
+	// Set the exported fields from the unexported
+	// inner type.
+	a.Name = "Bill"
+	a.Email = "bill@email.com"
+
+	fmt.Printf("User: %v\n", a)
+}
